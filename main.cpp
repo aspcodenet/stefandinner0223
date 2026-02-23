beb7bc0456fe23355ba026d89af5281365fdf845
Stefan Holmberg <stefan.holmberg@systementor.se>
2026-02-23 14:47:13 +0100

    game

--- main.cpp
+++ main.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+// 1. OOP Grunder - look for NOUNS
+// ShoppingCart
+// Product
+// Customer
+// ShoppingCart cart;
+// cart.add(product);
+// 2. structs vs classes vs objects
+/// CLASS = ritning (blueprint)
+// instansiera/bygga en SAK från en ritning
+//     => objekt
+
+// 3. Stefans OOP Dinner
+//          there are humans in Stefans house
+//          humans have
+//                       - level (0 initially)
+//                       - name
+//          humans can
+//                       - act (either eat, drink or burp)
+//                       - level up (if you burp three times in a row, you level up)
+// 4. Encapsulation
+// 5. In vector...
+
+// 6. INHERITANCE arv
+//         there are also ... FLIES in Stefans house
+
+
 class Human {
     std::string name;
     int level;
@@ -13,7 +42,7 @@ public:
 
       void act(){
         std::string actions[]={"eats","drinks","burps"};
-        int index = rand()%3;   
+        int index = rand()% 3; 
          std::cout << name << " " << actions[index] << std::endl;
       }
 
